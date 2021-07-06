d402e0a4f8bce3def5aec95f207196d85f161ad3
brain touch <brain_touch_whale@yahoo.co.jp>
2021-07-07 03:55:08 +0900

    Create Zikoku.java

---
+++ Zikoku.java
@@ -0,0 +1,13 @@
+/*     */ import java.util.Calendar;
+/*     */ 
+/*     */ class Zikoku {
+/*     */   public Zikoku() {
+/* 357 */     Calendar calendar = Calendar.getInstance();
+/* 359 */     long l = calendar.get(5);
+/* 360 */     int i = calendar.get(2);
+/* 361 */     int j = calendar.get(11);
+/* 362 */     int k = calendar.get(12);
+/* 363 */     int m = calendar.get(13);
+/*     */   }
+/*     */ }
+
